16e34fbe48b0ac1af2c1952cbbe117bb4c707739
Philip Porter <philaporter@gmail.com>
2020-07-25 00:09:17 -0700

    added simple deployment script and deployment config for sample app

---
+++ k8s setup/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# It hates me for wanting me for wanting to use my local image.
+# In order to get that done, I have to jump through the following hoops:
+# - Set the environment variables with eval $(minikube docker-env)
+# - Build the image with the Docker daemon of Minikube (eg docker build -t my-image .)
+# - Set the image in the pod spec like the build tag (eg my-image)
+# - Set the imagePullPolicy to Never, otherwise Kubernetes will try to download the image.
+# Important note: You have to run eval $(minikube docker-env) on each terminal you want to use, since it only sets the
+# environment variables for the current shell session.
+
+# kubectl create deployment first-k8s-deploy --image=“sample” -o yaml --dry-run > sample.yml
+# then I manually edited the file to have the updated imagePullPolicy
+
+echo "create deployment for the spring sample"
+eval $(minikube docker-env)
+docker build -t sample ../spring-sample/.
+kubectl create deployment sample --image=sample:latest
+kubectl get pods
